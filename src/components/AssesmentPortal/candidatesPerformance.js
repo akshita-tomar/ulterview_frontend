88ab72e5fb94ba1303a25216e5a4b4f6ee6ec5a6
shivam <shivam.ultivic@gmail.com>
2024-06-14 16:35:12 +0530

    update with build

--- src/components/AssesmentPortal/candidatesPerformance.js
+++ src/components/AssesmentPortal/candidatesPerformance.js
@@ -1,107 +1,117 @@
-import { useEffect, useState } from "react"
-import { Table } from "react-bootstrap";
-import swal from 'sweetalert'
-import { useNavigate } from 'react-router-dom'
-import { io } from 'socket.io-client'
+import { useEffect, useState } from "react";
+import { Table, Pagination } from "react-bootstrap";
+import swal from 'sweetalert';
+import { useNavigate } from 'react-router-dom';
+import { io } from 'socket.io-client';
 import { useAppContext } from "../../utils/useContext";
 
-
 const CandidatesPerformance = () => {
-    let url = process.env.REACT_APP_BACKEND_URL
-    let token = localStorage.getItem('token')
-    let navigate = useNavigate()
-    const [candidates, setCandidates] = useState([])
-    const socketurl = process.env.REACT_APP_SOCKET_URL
-    //  const socketurl = 'http://16.171.41.223:8000'
-    const socket = io(socketurl);
+  const url = process.env.REACT_APP_BACKEND_URL;
+  const token = localStorage.getItem('token');
+  const navigate = useNavigate();
+  const [candidates, setCandidates] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [totalPages, setTotalPages] = useState(1);
+  const itemsPerPage = 10;
+  const socketurl = process.env.REACT_APP_SOCKET_URL;
+  const socket = io(socketurl);
 
-    useEffect(() => {
-        const fetchCandidates = () => {
-            const myHeaders = new Headers();
-            myHeaders.append("Authorization", "Bearer " + token);
+  const fetchCandidates = (page) => {
+    const myHeaders = new Headers();
+    myHeaders.append("Authorization", "Bearer " + token);
 
-            const requestOptions = {
-                method: "GET",
-                headers: myHeaders,
-                redirect: "follow"
-            };
+    const requestOptions = {
+      method: "GET",
+      headers: myHeaders,
+      redirect: "follow"
+    };
 
-            fetch(`${url}getCandidatebyLanguage?languageId`, requestOptions)
-                .then((response) => response.json())
-                .then((result) => {
-                    console.log(result)
-                    if (result.type === 'success') {
-                        if (result.candidates.length < 1) {
-                            swal(
-                                {
-                                    confirmButtonColor: "red",
-                                    title: "Interview not completed by any candidate.",
-                                    text: "Thanks!",
-                                    icon: "success", className: "swal-button-custom",
-                                })
-                        } else {
-                            setCandidates(result.candidates)
-                        }}
-                })
-                .catch((error) => console.error(error));
+    fetch(`${url}getCandidatebyLanguage?languageId&page=${page}&limit=${itemsPerPage}`, requestOptions)
+      .then((response) => response.json())
+      .then((result) => {
+        if (result.type === 'success') {
+          if (result.candidates.length < 1) {
+            swal({
+              confirmButtonColor: "red",
+              title: "Interview not completed by any candidate.",
+              text: "Thanks!",
+              icon: "success",
+              className: "swal-button-custom",
+            });
+          } else {
+            setCandidates(result.candidates);
+            setTotalPages(Math.ceil(result.totalCount / itemsPerPage));
+          }
         }
+      })
+      .catch((error) => console.error(error));
+  };
 
-        fetchCandidates();
-        socket.on('interview_result_submitted', () => {
-            console.log("here in the socket-----")
-            fetchCandidates();
-        });
-        return () => {
-            socket.disconnect();
-        };
-    }, [])
+  useEffect(() => {
+    fetchCandidates(currentPage);
+    socket.on('interview_result_submitted', () => {
+      fetchCandidates(currentPage);
+    });
+    return () => {
+      socket.disconnect();
+    };
+  }, [currentPage]);
 
-
-    const handleExamine = (id, resultStatus) => {
-        if (resultStatus === 'selected' || resultStatus === 'rejected') {
-            navigate(`/candidate-checked-sheet/${id}`)
-        } else {
-            navigate(`/candidate-answers-sheet/${id}`)
-        }
-
-
+  const handleExamine = (id, resultStatus) => {
+    if (resultStatus === 'selected' || resultStatus === 'rejected') {
+      navigate(`/candidate-checked-sheet/${id}`);
+    } else {
+      navigate(`/candidate-answers-sheet/${id}`);
     }
-    const { show } = useAppContext()
+  };
 
-    return (
-        <div className={`wrapper ${show ? "cmn_margin" : ""}`}>
-            <div className="cmn_container">
-                <div className="table-responsive mb-0">
-                    <Table striped bordered hover className="user-table">
-                        <thead>
-                            <tr>
-                                <th>Sr.no</th>
-                                <th>Username</th>
-                                <th>Profile</th>
-                                <th>Experience</th>
-                                <th>Result Status</th>
-                                <th>Invite</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {candidates.map((element, index) => (
-                                <tr key={index}>
-                                    <td>{index + 1}</td>
-                                    <td>{element.username}</td>
-                                    <td>{element.profile}</td>
-                                    <td>{element.experience}</td>
-                                    <td className={element.resultStatus === 'rejected' ? 'rejected-candidate' : element.resultStatus === 'selected' ? 'selected-candidate' : ''}>
-                                        {element.resultStatus}
-                                    </td>
-                                    <td><button className="examin-btn invite_btn" onClick={() => handleExamine(element._id, element.resultStatus)}>Examine</button></td>
-                                </tr>
-                            ))}
-                        </tbody>
-                    </Table>
-                </div>
+  const handlePageChange = (pageNumber) => {
+    setCurrentPage(pageNumber);
+  };
 
-            </div>
+  const { show } = useAppContext();
+
+  return (
+    <div className={`wrapper ${show ? "cmn_margin" : ""}`}>
+      <div className="cmn_container">
+        <div className="table-responsive mb-0">
+          <Table striped bordered hover className="user-table">
+            <thead>
+              <tr>
+                <th>Sr.no</th>
+                <th>Username</th>
+                <th>Profile</th>
+                <th>Experience</th>
+                <th>Result Status</th>
+                <th>Invite</th>
+              </tr>
+            </thead>
+            <tbody>
+              {candidates.map((element, index) => (
+                <tr key={index}>
+                  <td>{(currentPage - 1) * itemsPerPage + index + 1}</td>
+                  <td>{element.username}</td>
+                  <td>{element.profile}</td>
+                  <td>{element.experience}</td>
+                  <td className={element.resultStatus === 'rejected' ? 'rejected-candidate' : element.resultStatus === 'selected' ? 'selected-candidate' : ''}>
+                    {element.resultStatus}
+                  </td>
+                  <td><button className="examin-btn invite_btn" onClick={() => handleExamine(element._id, element.resultStatus)}>Examine</button></td>
+                </tr>
+              ))}
+            </tbody>
+          </Table>
+          <Pagination>
+            {[...Array(totalPages).keys()].map(pageNumber => (
+              <Pagination.Item key={pageNumber} active={pageNumber + 1 === currentPage} onClick={() => handlePageChange(pageNumber + 1)}>
+                {pageNumber + 1}
+              </Pagination.Item>
+            ))}
+          </Pagination>
         </div>
-    )
-}
-export default CandidatesPerformance
+      </div>
+    </div>
+  );
+};
+
+export default CandidatesPerformance;
